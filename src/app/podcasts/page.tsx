7792be04537c57bdc322bd299e83fa14e9c011a3
Daniel Lopez <d@niel.to>
2024-08-15 11:29:53 -0700

    fetch favorites in podcasts page to fix build error

--- src/app/podcasts/page.tsx
+++ src/app/podcasts/page.tsx
@@ -1,12 +1,14 @@
 import { Main, Title } from '@/components/ui'
+import { getFavorites } from '@/server/queries'
 import Search from '../_components/search'
 
-export default function PodcastsPage() {
+export default async function PodcastsPage() {
+  const favorites = await getFavorites()
   return (
     <Main className='flex flex-col p-4'>
       <div className='flex w-full flex-grow flex-col items-center space-y-4'>
         <Title>podz</Title>
-        <Search />
+        <Search favorites={favorites} />
       </div>
     </Main>
   )
