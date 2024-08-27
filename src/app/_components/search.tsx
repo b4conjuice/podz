ebfefd9729b0ca0eef4aa0303bc043d4ef1d3932
Daniel Lopez <d@niel.to>
2024-08-27 17:58:32 -0600

    refactor search to use combobox

--- src/app/_components/search.tsx
+++ src/app/_components/search.tsx
@@ -1,11 +1,20 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import Link from 'next/link'
 import { useRouter, useSearchParams } from 'next/navigation'
-import { HeartIcon as OutlineHeartIcon } from '@heroicons/react/24/outline'
+import {
+  MagnifyingGlassIcon,
+  HeartIcon as OutlineHeartIcon,
+} from '@heroicons/react/24/outline'
 import { HeartIcon as SolidHeartIcon } from '@heroicons/react/24/solid'
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+} from '@headlessui/react'
 import { useDebounce } from '@uidotdev/usehooks'
+import classNames from 'classnames'
 
 import useSearch from '@/lib/useSearch'
 import fetcher from '@/lib/fetcher'
@@ -17,7 +26,7 @@ export default function Search({ favorites }: { favorites: Podcast[] }) {
   const searchParams = useSearchParams()
   const query = searchParams.get('q')
   const [results, setResults] = useState<Podcast[]>([])
-  const { search, setSearch, searchRef } = useSearch({
+  const { search, setSearch } = useSearch({
     initialSearch: '',
     options: {
       keys: ['title', 'body'],
@@ -45,56 +54,80 @@ export default function Search({ favorites }: { favorites: Podcast[] }) {
   return (
     <>
       <h2>search</h2>
-      <input
-        ref={searchRef}
-        type='text'
-        className='w-full bg-cb-blue'
-        placeholder='enter a podcast name'
-        value={search}
-        onChange={e => {
-          const { value } = e.target
-          setSearch(value)
-          const url = `/${value ? `?q=${value}` : ''}`
-          router.push(url)
+      <Combobox
+        as='div'
+        onChange={(podcast: Podcast) => {
+          router.push(`/podcasts/${podcast.trackId}`)
         }}
-      />
-      {results?.length && results?.length > 0 ? (
-        <ul className='divide-y divide-cb-dusty-blue'>
-          {results.map(result => (
-            <li key={result.trackId} className='flex py-4 first:pt-0'>
-              <Link
-                href={`/podcasts/${result.trackId}`}
-                className='grow text-cb-pink hover:text-cb-pink/75'
-              >
-                {result.trackName}
-              </Link>
-              {favorites.find(f => f.trackId === result.trackId) ? (
-                <form
-                  action={async () => {
-                    await deleteFavorite(result.trackId)
+        className='relative mx-auto w-full divide-y divide-cb-dusty-blue overflow-hidden rounded-xl bg-cb-blue ring-1 ring-cb-mint'
+      >
+        <div className='flex items-center space-x-2 px-4'>
+          <MagnifyingGlassIcon className='h-6 w-6 text-cb-yellow' />
+          <ComboboxInput
+            onChange={e => {
+              const { value } = e.target
+              setSearch(value)
+              const url = `/${value ? `?q=${value}` : ''}`
+              router.push(url)
+            }}
+            className='h-12 w-full border-0 bg-transparent placeholder-cb-yellow/75 focus:outline-0 focus:ring-0'
+            placeholder='enter a podcast name'
+            value={search}
+          />
+        </div>
+        {results.length > 0 && (
+          <ComboboxOptions static className='max-h-40 overflow-y-auto py-4'>
+            {results.map(result => {
+              return (
+                <ComboboxOption key={result.trackId} value={result}>
+                  {({ active }) => {
+                    return (
+                      <div
+                        className={classNames(
+                          'flex space-x-1 px-4 py-2',
+                          active ? 'bg-cb-yellow' : 'bg-cb-blue'
+                        )}
+                      >
+                        <span
+                          className={classNames(
+                            'grow font-medium',
+                            active ? 'text-cb-blue' : ''
+                          )}
+                        >
+                          {result.trackName}
+                        </span>
+                        {favorites.find(f => f.trackId === result.trackId) ? (
+                          <form
+                            action={async () => {
+                              await deleteFavorite(result.trackId)
+                            }}
+                          >
+                            <button type='submit' className='group'>
+                              <SolidHeartIcon className='h-6 w-6 text-cb-pink group-hover:hidden' />
+                              <OutlineHeartIcon className='hidden h-6 w-6 text-cb-pink group-hover:block' />
+                            </button>
+                          </form>
+                        ) : (
+                          <form
+                            action={async () => {
+                              await addFavorite(result)
+                            }}
+                          >
+                            <button type='submit' className='group'>
+                              <OutlineHeartIcon className='h-6 w-6 text-cb-pink group-hover:hidden' />
+                              <SolidHeartIcon className='hidden h-6 w-6 text-cb-pink group-hover:block' />
+                            </button>
+                          </form>
+                        )}
+                      </div>
+                    )
                   }}
-                >
-                  <button type='submit' className='group'>
-                    <SolidHeartIcon className='h-6 w-6 text-cb-pink group-hover:hidden' />
-                    <OutlineHeartIcon className='hidden h-6 w-6 text-cb-pink group-hover:block' />
-                  </button>
-                </form>
-              ) : (
-                <form
-                  action={async () => {
-                    await addFavorite(result)
-                  }}
-                >
-                  <button type='submit' className='group'>
-                    <OutlineHeartIcon className='h-6 w-6 text-cb-pink group-hover:hidden' />
-                    <SolidHeartIcon className='hidden h-6 w-6 text-cb-pink group-hover:block' />
-                  </button>
-                </form>
-              )}
-            </li>
-          ))}
-        </ul>
-      ) : null}
+                </ComboboxOption>
+              )
+            })}
+          </ComboboxOptions>
+        )}
+      </Combobox>
     </>
   )
 }
