6752af5f4491d33d5771ad8af312499991e53a42
Daniel Lopez <d@niel.to>
2024-10-16 20:18:46 -0700

    check if user logged in when viewing note page

--- src/app/notes/[id]/page.tsx
+++ src/app/notes/[id]/page.tsx
@@ -1,8 +1,18 @@
+import { auth } from '@clerk/nextjs/server'
+
 import { Main } from '@/components/ui'
 import Textarea from '@/app/_components/textarea'
 import { getNote } from '@/server/queries'
 
 export default async function NotePage({ params }: { params: { id: string } }) {
+  const user = auth()
+  if (!user.userId) {
+    return (
+      <Main className='container mx-auto flex max-w-screen-md flex-col px-4 md:px-0'>
+        <p>you must be logged in to view this note</p>
+      </Main>
+    )
+  }
   const note = await getNote(Number(params.id))
   if (!note) {
     return (
