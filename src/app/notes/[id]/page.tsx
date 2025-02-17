ccbccbf403dfc643e3d5dea1c7f703642b0b2b31
Daniel Lopez <d@niel.to>
2025-02-17 14:22:56 -0800

    feat(note): change topnav title to note title (podcast title)

--- src/app/notes/[id]/page.tsx
+++ src/app/notes/[id]/page.tsx
@@ -11,22 +11,28 @@ export default async function NotePage({ params }: { params: { id: string } }) {
   const user = auth()
   if (!user.userId) {
     return (
-      <Main className='container mx-auto flex max-w-screen-md flex-col px-4 md:px-0'>
-        <p>you must be logged in to view this note</p>
-      </Main>
+      <>
+        <TopNav />
+        <Main className='container mx-auto flex max-w-screen-md flex-col px-4 md:px-0'>
+          <p>you must be logged in to view this note</p>
+        </Main>
+      </>
     )
   }
   const note = await getNote(Number(params.id))
   if (!note) {
     return (
-      <Main className='container mx-auto flex max-w-screen-md flex-col px-4 md:px-0'>
-        <p>no note found</p>
-      </Main>
+      <>
+        <TopNav />
+        <Main className='container mx-auto flex max-w-screen-md flex-col px-4 md:px-0'>
+          <p>no note found</p>
+        </Main>
+      </>
     )
   }
   return (
     <>
-      <TopNav />
+      <TopNav title={note.title} />
       <Main className='container mx-auto flex max-w-screen-md flex-col'>
         <div className='flex w-full flex-grow flex-col space-y-4'>
           {/* <Link // TODO: Maybe add back podcast link?
@@ -35,7 +41,6 @@ export default async function NotePage({ params }: { params: { id: string } }) {
         >
           {podcast.trackName}
         </Link> */}
-          <h2 className='px-4'>{note.title}</h2>
           <Textarea
             note={{
               id: note.id,
